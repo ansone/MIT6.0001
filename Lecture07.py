f47c180213c500f2a397bbd9be34a9f7be8d4f57
André das Neves <ansone@users.noreply.github.com>
2018-05-09 17:56:58 -0300

    Create Lecture07.py

---
+++ Lecture07.py
@@ -0,0 +1,18 @@
+""""
+#In program:
+try":
+  <code>
+exception (ValueError, TypeError):
+  print("error")
+else:
+  <do something not handled or handled by 'exception' statement only>
+finally":
+  <do something always>
+"""
+
+"""
+#Inside functions:
+#raise <exceptionName>(<argumenst>)
+raise ValueError("somethong is wrong")
+raise Exception("something is wrong")
+"""
